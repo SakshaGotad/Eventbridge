4d9126b4ef62a8acf66bb179b7dab6de342421d7
SakshaGotad <sakshagotad@gmail.com>
2026-03-19 21:42:16 +0530

    feat: Implement BullMQ-based queueing and worker system for asynchronous workflow execution.

--- backend/libs/core/src/eventbridge.module.ts
+++ backend/libs/core/src/eventbridge.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { CoreService } from './eventbridge.service';
 import { EngineModule } from './engine/engine.module';
 import { WorkflowModule } from './workflow/workflow.module';
@@ -6,10 +6,26 @@ import { ExecutorModule } from './executor/executor.module';
 import { QueueModule } from './queue/queue.module';
 import { WorkflowService } from './workflow/workflow.service';
 import { DatabaseModule } from './storage/database.module';
+import { createWorker } from './queue/worker';
+import { ExecutorService } from './executor/executor.service';
+import { WorkflowRunRepository } from './storage/workflow-run.repository';
 
 @Module({
   providers: [CoreService,WorkflowService],
   exports: [CoreService,WorkflowService],
   imports: [EngineModule, WorkflowModule, ExecutorModule, QueueModule, DatabaseModule],
+  
 })
-export class EventbridgeModule { }
+export class EventbridgeModule implements OnModuleInit { 
+  constructor(
+    private readonly executor: ExecutorService,
+    private readonly workflowService: WorkflowService,
+    private readonly workflowRunRepo: WorkflowRunRepository,
+  ){}
+   onModuleInit() {
+    createWorker(
+      this.executor,
+      this.workflowService,
+      this.workflowRunRepo
+    );}
+}
--- backend/libs/core/src/queue/queue.service.ts
+++ backend/libs/core/src/queue/queue.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { Queue } from 'bullmq';
+import IORedis from 'ioredis';
 @Injectable()
-export class QueueService {}
+export class QueueService {
+  private queue: Queue;
+
+  constructor() {
+    this.queue = new Queue('eventbridge-workflow', {
+      connection: {
+        url: process.env.REDIS_URL!,
+      },
+    });
+
+  }
+  async addWorkflowJob(data: any) {
+    await this.queue.add('run-workflow', data);
+  }
+}
+
